5bd4d8fbdf1655ec21d64448e5d54e93f7756531
Mateus Moog <mateusmoog@gmail.com>
2017-03-26 02:45:50 -0300

    ok

--- controllers/buyer.js
+++ controllers/buyer.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
     const BuyerModel = app.models.buyer;
     
     return {
-        get: async function(req, res) {
+        getAll: async function(req, res) {
             try {
                 const buyers = await BuyerModel.find();
                 res.json({ success: true, buyers });
