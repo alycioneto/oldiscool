413890e9bba188aea8b592b752a8910c8665379e
Mateus Moog <mateusmoog@gmail.com>
2017-03-26 15:38:41 -0300

    oi

--- front/github.js
+++ front/github.js
@@ -2,7 +2,7 @@
   angular.module("saoVicentinoApp")
   .factory('github', ['$http', function githubFactory($http) {
      var getUser = function(){
-       return $http.get("http://oldiscool.herokuapp.com/api/events")
+       return $http.get("http://localhost/course1/finalAndCorrect/json")
      };
     return{ //what i return will represent the public api
       getUser: getUser
