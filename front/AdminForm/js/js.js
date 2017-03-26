27947065bd6645df8062c3288b84441b16af5cc1
Mateus Moog <mateusmoog@gmail.com>
2017-03-26 14:04:56 -0300

    oi

--- front/AdminForm/js/js.js
+++
@@ -1,9 +0,0 @@
-
-function validacaoForm(){
-  var senha = document.forms['formAdmin']['password'].value;
-  var email = document.forms['formAdmin']['email'].value;
-  if (senha != 'senha' || email != 'email@email.com') {
-      alert("Erro, senha/email incorretos");
-      return false;
-  }
-}
