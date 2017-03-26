994227e1b469a94cf9cd9f04b6b8199d282c9e06
Mateus Moog <mateusmoog@gmail.com>
2017-03-26 02:39:13 -0300

    Buyers and events gets

--- controllers/event.js
+++ controllers/event.js
@@ -5,11 +5,57 @@ module.exports = function(app) {
         getFuture: async function(req, res) {
             const today = new Date();
             const query = { startDate: { gte: today } };
+            const limit = req.query.limit;
+            let findEvents = EventModel.find(query);
+
+            if(limit && Number.isInteger(limit)) {
+                findEvents = findEvents.limit(limit);
+            }
 
             try {
-                const events = await EventModel.find(query).exec();
+                const events = await findEvents.exec();
                 res.json({ success: true, events });
             } catch(err) {
+                res.json({ success: false, messages: err.errors });
+            }
+        },
+        getPast: async function(req, res) {
+            const today = new Date();
+            const query = { endDate: { lte: today } };
+            const limit = req.query.limit;
+            let findEvents = EventModel.find(query);
+
+            if(limit && Number.isInteger(limit)) {
+                findEvents = findEvents.limit(limit);
+            }
+
+            try {
+                const events = await findEvents.exec();
+                res.json({ success: true, events });
+            } catch (err) {
+                res.json({ success: false, messages: err.errors });
+            }
+        },
+        getAll: async function(req, res) {
+            const limit = req.query.limit;
+            let findEvents = EventModel.find();
+
+            if(limit && Number.isInteger(limit)) {
+                findEvents = findEvents.limit(limit);
+            }
+
+            try {
+                const events = await findEvents.exec();
+                res.json({ success: true, events });
+            } catch (err) {
+                res.json({ success: false, messages: err.errors });
+            }
+        },
+        getOne: async function(req, res) {
+            try {
+                const event = await EventModel.findById(req.params.id).exec()
+                res.json({ success: true, event });
+            } catch (err) {
                 res.json({ success: false, messages: err.errors });
             }
         }
--- routes/event.js
+++ routes/event.js
@@ -1,5 +1,8 @@
 module.exports = function(app) {
     const controller = app.controllers.event;
 
-    app.get('/api/events', controller.getFuture);
+    app.get('/api/events', controller.getAll);
+    app.get('/api/events/future', controller.getFuture);
+    app.get('/api/events/past', controller.getPast);
+    app.get('/api/event/:id', controller.getOne)
 }
