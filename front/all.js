e8d21716f3712d0bcded90dc16ea51ee3d29b5f9
Mateus Moog <mateusmoog@gmail.com>
2017-03-26 11:36:13 -0300

    bla

---
+++ front/all.js
@@ -0,0 +1,28 @@
+
+
+ 	  $(document).ready(function() {
+
+	    $(window).scroll(function () {
+	        if (window.scrollY > 200) {
+	            $('.nav-overlay').addClass('fixed-nav');
+	            $('.nav-overlay').addClass('aparece');
+	            $('.isHidden').removeClass('isHidden');
+
+	        } else {
+	            $('.nav-overlay').removeClass('fixed-nav');
+	             $('.nav-overlay').addClass('esconde');
+	             $('.nav-overlay').removeClass('aparece');
+	              $('.add').addClass('isHidden');
+	        }
+
+	        if (window.scrollY > $('nav').outerHeight()) {
+	            $('.nav-overlay').css('top', -100);
+	        } else {
+	            $('.nav-overlay').css('top', 0);
+	        }
+	    });
+
+	});
+
+
+	   
