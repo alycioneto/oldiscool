db050218aca6ac6d0e67686be097589c814ef6e0
Mateus Moog <mateusmoog@gmail.com>
2017-03-26 01:22:55 -0300

    Future events

--- controllers/event.js
+++ controllers/event.js
@@ -1,8 +1,17 @@
 module.exports = function(app) {
+    const EventModel = app.models.event;
 
     return {
-        getAll: function(req, res) {
-            return res.json({ success: true })
+        getFuture: async function(req, res) {
+            const today = new Date();
+            const query = { startDate: { gte: today } };
+
+            try {
+                const events = await EventModel.find(query).exec();
+                res.json({ success: true, events });
+            } catch(err) {
+                res.json({ success: false, messages: err.errors });
+            }
         }
     };
 };
